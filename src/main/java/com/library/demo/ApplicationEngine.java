ea21512a0d19d3bbbf05277872e5a83b1ee3aa9b
ElTosheva <elisaveta.tosheva@gmail.com>
2023-06-18 15:37:13 +0300

    Formating

--- src/main/java/com/library/demo/ApplicationEngine.java
+++ src/main/java/com/library/demo/ApplicationEngine.java
@@ -1,12 +1,10 @@
 package com.library.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ApplicationEngine {
@@ -19,7 +17,7 @@ public class ApplicationEngine {
 
     public void run() {
         Map<String, Object> filledData = new HashMap<>();
-        if(persistentStorage == null) {
+        if (persistentStorage == null) {
             filledData = fillDumpDate();
         }
         filledData.forEach((key, value) -> System.out.println(key + " " + value.toString()));
@@ -31,7 +29,7 @@ public class ApplicationEngine {
         persistentMap.put("keyTwo", new Book(2, "The Road"));
         persistentMap.put("keyThree", new Book(3, "World War Z: An Oral History of the Zombie War"));
         persistentMap.put("keyFour", new Book(4, "Deep Wheel Orcadia: A Novel"));
-        persistentMap.put("keyFive", new Book(5 ,"A River Called Time"));
+        persistentMap.put("keyFive", new Book(5, "A River Called Time"));
         persistentMap.put("keyNull", new Book(0, "SNull"));
         return persistentMap;
     }
